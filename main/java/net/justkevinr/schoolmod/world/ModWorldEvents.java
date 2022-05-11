f270e508fe6baa97524bfaad2e099946edf50923
JustKevinR <36241579+JustKevinR@users.noreply.github.com>
2022-05-11 18:36:44 +0300

    Oregen & fixed ore dorps

---
+++ main/java/net/justkevinr/schoolmod/world/ModWorldEvents.java
@@ -0,0 +1,16 @@
+package net.justkevinr.schoolmod.world;
+
+
+import net.justkevinr.schoolmod.SchoolMod;
+import net.justkevinr.schoolmod.world.gen.ModOreGeneration;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SchoolMod.MOD_ID)
+public class ModWorldEvents {
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+        ModOreGeneration.generateOres(event);
+    }
+}
